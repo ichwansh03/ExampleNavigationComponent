9daf10ae90d7e70e40cedadc2d952129c7442cfa
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-05 20:32:00 +0700

    chore : add navigate in ForgotPasswordFragment

--- app/src/main/java/com/ichwan/arch/navigationcomponent/authentication/ForgotPasswordFragment.kt
+++ app/src/main/java/com/ichwan/arch/navigationcomponent/authentication/ForgotPasswordFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ichwan.arch.navigationcomponent.R
+import com.ichwan.arch.navigationcomponent.databinding.FragmentForgotPasswordBinding
 
 class ForgotPasswordFragment : Fragment() {
+
+    private var _binding: FragmentForgotPasswordBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    ): View {
+        _binding = FragmentForgotPasswordBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonToLogin.setOnClickListener {
+            findNavController().navigate(R.id.forgot_to_login)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
