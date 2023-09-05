e025e8acde29ec802f07eaff0270c74bfa139267
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-05 22:36:04 +0700

    chore : setup navigation menu

--- app/src/main/java/com/ichwan/arch/navigationcomponent/menu/MainActivity.kt
+++ app/src/main/java/com/ichwan/arch/navigationcomponent/menu/MainActivity.kt
@@ -2,11 +2,26 @@ package com.ichwan.arch.navigationcomponent.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.ichwan.arch.navigationcomponent.R
+import com.ichwan.arch.navigationcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
     }
 }
