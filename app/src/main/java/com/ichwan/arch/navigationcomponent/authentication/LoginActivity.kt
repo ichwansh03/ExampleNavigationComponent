72cef525307ed40a7cedd16394bd5994171ca040
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-05 17:02:06 +0700

    starter nav project for fragment and bottom nav

---
+++ app/src/main/java/com/ichwan/arch/navigationcomponent/authentication/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ichwan.arch.navigationcomponent.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ichwan.arch.navigationcomponent.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/ichwan/arch/navigationcomponent/menu/MainActivity.kt
+++ app/src/main/java/com/ichwan/arch/navigationcomponent/menu/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ichwan.arch.navigationcomponent
+package com.ichwan.arch.navigationcomponent.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ichwan.arch.navigationcomponent.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
